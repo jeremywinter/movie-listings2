f6152a9c46a7d870f38db116518451b2d878427d
Jeremy Winter <jeremywinter@users.noreply.github.com>
2022-06-14 16:18:42 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { useEffect, useState } from "react";
+import Movie from './components/Movies';
+import Filter from './components/Filter';
+
+function App() {
+
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => { 
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=5dbcfaf024854acb4c09228ce0f00ecd&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <Filter 
+        popular={popular} 
+        setFiltered={setFiltered} 
+        activeGenre={activeGenre} 
+        setActiveGenre={setActiveGenre} 
+      />
+     <div className="popular-movies">
+       {filtered.map((movie) => {
+         return <Movie key={movie.id} movie={movie} />;
+       })}
+     </div>
+    </div>
+  );
+}
+
+export default App;
